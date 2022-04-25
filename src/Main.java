898498e6b49c2b5a35f28e827548b85219e9e41f
Alexey Zimin <lexx.jock@gmail.com>
2022-04-25 19:09:42 +1000

    commit qajava-2.1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketAmount = 5799;
+        int priceBonus = 20;
+        int bonusMile = ticketAmount / priceBonus;
+        System.out.println("Количество бонусных миль = " + bonusMile);
+    }
+
+}
